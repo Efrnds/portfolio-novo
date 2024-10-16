8ec5281548b437f4138bb2c3b1d19168de0828f1
Eduardo fernandes da silva <eduardo.fernandes.silva13@gmail.com>
2024-10-16 17:47:28 -0300

    zapp

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -6,8 +6,8 @@ function Main() {
         <main className="flex flex-col justify-center p-5 sm:p-10">
             <About />
             <p className="mx-auto text-2xl text-red-600 ">Warning! The website is a Work in Progress</p>
-            <Project title="Sakura Verse" path="/Y" years="2023" image1="/images/Desktop - 1.png" image2="/images/Desktop - 1.png" image3="/images/Desktop - 1.png"/>
-            <Project title="Apple" path="/Kitchen" years="2021_2023" image1="/images/hannaxu.com_apple_.png" image2="/images/hannaxu.com_apple_.png" image3="/images/hannaxu.com_apple_.png"/>
+            <Project title="Sakura Verse" path="/Y" years="2023" image1="/images/mobile.png" image2="/images/notebook.webp" image3="/images/notebook.webp"/>
+            <Project title="Apple" path="/Kitchen" years="2021_2023" image1="/images/mobile.png" image2="/images/notebook.webp" image3="/images/notebook.webp"/>
         </main>
     );
 }

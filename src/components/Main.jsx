479e5f0c268ce74e39e840ae290ef23016b7a525
Eduardo fernandes da silva <edudfrs@gmail.com>
2024-12-13 00:38:31 +0000

    removido o warning

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,7 +5,7 @@ function Main() {
     return (
         <main className="flex flex-col justify-center h-full p-5 sm:p-10">
             <About />
-            <p className="mx-auto text-2xl text-red-600 ">Warning! The website is a Work in Progress</p>
+            {/* <p className="mx-auto text-2xl text-red-600 ">Warning! The website is a Work in Progress</p> */}
             <Project title="Y" path="/Y" years="2024" image1="/images/y-mobile.png" image2="/images/y-desktop.png" image3="/images/y-tablet.png"/>
             <Project title="Property System" path="/sistema-patrimonio" years="2023_2024" image1="/images/pat-mobile.png" image2="/images/pat-notebook.png" image3="/images/pat-ipad.png"/>
         </main>
